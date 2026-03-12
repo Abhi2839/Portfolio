a9155d6f2723595bcdb71d26b6a4c2e8c5d2e0dc
abhey garg <143724189+Abhi2839@users.noreply.github.com>
2026-03-12 22:31:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const toggle=document.getElementById("theme-toggle");
+
+toggle.onclick=()=>{
+
+document.body.classList.toggle("light");
+
+if(document.body.classList.contains("light")){
+toggle.innerHTML="☀️";
+}else{
+toggle.innerHTML="🌙";
+}
+
+};
