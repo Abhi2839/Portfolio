69ef7ff9286c29ba04696696b2e1c2adff658a54
Abhey <gabhey2005@gmail.com>
2026-03-16 18:51:37 +0530

    Update the code

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const toggle=document.getElementById("theme-toggle");
 
 toggle.onclick=()=>{
@@ -6,59 +5,13 @@ toggle.onclick=()=>{
 document.body.classList.toggle("light");
 
 if(document.body.classList.contains("light")){
+
 toggle.innerHTML="☀️";
+
 }else{
+
 toggle.innerHTML="🌙";
+
 }
 
 };
-
-const cards=document.querySelectorAll(".about-card");
-
-window.addEventListener("scroll",()=>{
-
-cards.forEach(card=>{
-
-const cardTop=card.getBoundingClientRect().top;
-
-if(cardTop < window.innerHeight - 100){
-
-card.classList.add("show");
-
-}
-
-});
-
-=======
-const toggle=document.getElementById("theme-toggle");
-
-toggle.onclick=()=>{
-
-document.body.classList.toggle("light");
-
-if(document.body.classList.contains("light")){
-toggle.innerHTML="☀️";
-}else{
-toggle.innerHTML="🌙";
-}
-
-};
-
-const cards=document.querySelectorAll(".about-card");
-
-window.addEventListener("scroll",()=>{
-
-cards.forEach(card=>{
-
-const cardTop=card.getBoundingClientRect().top;
-
-if(cardTop < window.innerHeight - 100){
-
-card.classList.add("show");
-
-}
-
-});
-
->>>>>>> f0d5e0ca46282b2dbdd5fec75e91bcf16d23d932
-});
