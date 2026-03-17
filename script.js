1132287b7a6708777d26c791e813666e117b3dc5
Abhey <gabhey2005@gmail.com>
2026-03-17 10:16:45 +0530

    updated the code

--- script.js
+++ script.js
@@ -15,3 +15,18 @@ toggle.innerHTML="🌙";
 }
 
 };
+
+const observer = new IntersectionObserver(entries => {
+
+entries.forEach(entry => {
+if(entry.isIntersecting){
+entry.target.classList.add("show");
+}
+});
+
+});
+
+document.querySelectorAll("section").forEach(sec=>{
+sec.classList.add("hidden");
+observer.observe(sec);
+});
